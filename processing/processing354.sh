58d9f5de6ef79d8c726a0a658f3df1423da7735d
melongist <what_is_computer@msn.com>
2021-12-26 08:30:42 +0900

    2021.12.26 update
    2021.12.26 update

--- processing/processing354.sh
+++ processing/processing354.sh
@@ -11,20 +11,19 @@ sudo apt -y autoremove
 #processing version check
 #https://processing.org/download
 
-#download
-wget https://github.com/processing/processing/releases/download/processing-0270-3.5.4/processing-3.5.4-linux64.tgz
-
-#install
+#3.5.4
+get https://github.com/processing/processing/releases/download/processing-0270-3.5.4/processing-3.5.4-linux64.tgz
 sudo tar -xvf processing-3.5.4-linux64.tgz
 sudo mv ./processing-3.5.4 ./processing
 sudo mv ./processing /opt/.
 sudo bash /opt/processing/install.sh
-
-#file remove
 sudo rm processing-3.5.4-linux64.tgz
 
 #korean font
 sudo apt -y install fonts-nanum
+
+#bug fix
+#https://github.com/processing/processing/issues/5308#issuecomment-367534475
 
 #reboot
 clear
