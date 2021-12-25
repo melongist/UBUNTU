7c6ee84af02c0950455792d4bd76501c74fa546f
melongist <what_is_computer@msn.com>
2021-12-25 17:29:45 +0900

    2021.12.25 update
    2021.12.25 update

---
+++ HWP/hoffice112001520.sh
@@ -0,0 +1,29 @@
+#hancom HWP installation
+#for Ubuntu Desktop 20.04.3
+
+#referenced
+#https://dawoum.ddns.net/wiki/Installing_hoffice_2022_beta_on_Ubuntu
+
+sudo apt update
+sudo apt -y upgrade
+
+sudo apt -y install curl
+
+#deb file version check
+#https://github.com/hancomgooroom/hancom-toolkit/blob/hancom-2.0/src/resources/hancom-toolkit.json
+
+#download
+curl -H "Host: cdn.hancom.com" -H "Referer: https://www.hancom.com/cs_center" -fLO https://cdn.hancom.com/pds/hnc/DOWN/gooroom/hoffice_11.20.0.1520_amd64.deb
+
+#install
+sudo apt install ./hoffice_11.20.0.1520_amd64.deb
+
+#file remove
+sudo rm hoffice_11.20.0.1520_amd64.deb
+
+#keyboard enter error ( fix
+gsettings set org.freedesktop.ibus.engine.hangul use-event-forwarding false
+
+#reboot
+sudo reboot
+
