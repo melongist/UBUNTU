44c374c56a7cd26bcee43c5bfbb7d03e76c75816
Hyun-seok Jeon <92401080+melongist@users.noreply.github.com>
2022-04-29 07:54:29 +0900

    22.04.29 update
    22.04.29 update

--- arduino/arduino1819.sh
+++ arduino/arduino1819.sh
@@ -10,4 +10,24 @@ sudo rm -rf ./arduino-1.8.19-linux64.tar.xz
 sudo ./arduino-1.8.19/install.sh
 sudo rm -rf ./arduino-1.8.19
 
-sudo usermod -a -G dialout $SUDO_USER
+sudo usermod -a -G dialout $USER
+
+#reboot
+clear
+
+echo ''
+echo 'arduino 1.8.19 Linux 64 bits'
+echo 'for Ubuntu Desktop 20.04 installed!'
+echo ''
+echo 'system will be rebooted!'
+echo ''
+COUNT=5
+while [ $COUNT -ge 0 ]
+do
+  echo $COUNT
+  ((COUNT--))
+  sleep 1
+done
+echo 'rebooted!'
+sleep 1
+sudo reboot
