c7582df3c59ef2a226cf79b8f0082e012edfb1a5
Hyun-seok Jeon <92401080+melongist@users.noreply.github.com>
2022-04-29 08:02:10 +0900

    22.04.29 update
    22.04.29 update

--- arduino/arduino1819.sh
+++ arduino/arduino1819.sh
@@ -5,10 +5,12 @@ sudo apt -y upgrade
 
 wget https://downloads.arduino.cc/arduino-1.8.19-linux64.tar.xz
 sudo tar -xvf ./arduino-1.8.19-linux64.tar.xz
+sudo mv ./arduino-1.8.19-linux64 ./arduino
+sudo mv ./arduino /opt/.
+
 sudo rm -rf ./arduino-1.8.19-linux64.tar.xz
 
-sudo ./arduino-1.8.19/install.sh
-sudo rm -rf ./arduino-1.8.19
+sudo bash /opt/arduino/install.sh
 
 sudo usermod -a -G dialout $USER
 
