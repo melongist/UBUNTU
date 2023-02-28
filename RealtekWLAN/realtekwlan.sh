9ece7e2ce0d9c345bb56158889f1cb004a2c9c33
melongist <what_is_computer@msn.com>
2023-02-28 09:44:47 +0900

    23.02.28 update

--- RealtekWLAN/realtekwlan.sh
+++ RealtekWLAN/realtekwlan.sh
@@ -23,10 +23,10 @@ fi
 
 #sudo apt install -y git
 #git clone https://github.com/HRex39/rtl8852be.git -b dev
-#tar -czvpf ./rtl8852be.tar.gz ./rtl8852be
+#sudo tar -czvpf ./rtl8852be.tar.gz ./rtl8852be
 
-wget https://raw.githubusercontent.com/melongist/CSL/master/UBUNTU/realtekwlan/rtl8852be.tar.gz
-tar -xzvpf ./rtl8852be.tar.gz
+wget https://raw.githubusercontent.com/melongist/UBUNTU/master/RealtekWLAN/rtl8852be.tar.gz
+sudo tar -xzvpf ./rtl8852be.tar.gz
 
 cd rtl8852be
 
